d422d561f777e0d97f5ea08b99f760ef9df5fceb
최승표 <cspyo98@naver.com>
2023-09-02 21:03:15 +0900

    feat: add config service for db

--- pyolog/src/app.module.ts
+++ pyolog/src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const typeOrmModuleOptions = {
+  useFactory: async (
+    configService: ConfigService,
+  ): Promise<TypeOrmModuleOptions> => ({
+    type: 'postgres',
+    host: configService.get('POSTGRES_HOST'),
+    port: configService.get('POSTGRES_PORT'),
+    username: configService.get('POSTGRES_USER'),
+    password: configService.get('POSTGRES_PASSWORD'),
+    database: configService.get('POSTGRES_DB'),
+    entities: [],
+    // set this options
+    synchronize: true,
+    logging: true,
+    autoLoadEntities: true,
+    keepConnectionAlive: true,
+  }),
+  inject: [ConfigService],
+};
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRootAsync(typeOrmModuleOptions)],
   controllers: [AppController],
   providers: [AppService],
 })
